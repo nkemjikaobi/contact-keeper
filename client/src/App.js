474af1eb2fbeb16616dd6f1936ea398b25b30c87
nkemjikaobi <nkemjikaobi@gmail.com>
2021-02-08 12:47:26 +0100

    Navbar and Router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,23 @@
+import React, { Fragment } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Navbar from './components/layouts/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Fragment className="App">
+        <Navbar />
+        <div className="container">
+          <Switch>
+              <Route exact path='/' component={ Home } />
+              <Route exact path='/about' component={ About } />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
   );
 }
 
